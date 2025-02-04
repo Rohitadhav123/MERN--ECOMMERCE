425eae6326104c023644acb47dbf0f3b8b801120
Rohitadhav123 <159225705+Rohitadhav123@users.noreply.github.com>
2025-02-04 08:07:01 +0530

    server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import authcontriler from './routes/auth.js'
 import cors from 'cors';
 import categoryRoute from './routes/cateogryroute.js' 
 import productRoute from './routes/productRoutes.js'
-import path, { dirname } from 'path';
+
 dotenv.config()
 const app=express()
 const  port=process.env.PORT || 8000
@@ -16,7 +16,7 @@ connectDB()
 
 // MIDDLEWERE
 app.use(express.json());
-app.use(express.static(path.join(__dirname,'./client/build')))
+
 
 
 // routes
@@ -24,9 +24,7 @@ app.use('/api/v1/auth', authcontriler )
 app.use('/api/v1/category',categoryRoute)
 app.use('/api/v1/product',productRoute)
 
-app.use('*',function(req,res){
-res.sendFile(path.join(__dirname,'./client/build/index.html'))
-})
+
 
 app.listen(port,()=>{
     console.log(`It is workin on port no ${port}`)
